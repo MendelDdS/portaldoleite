e92a9e534f71f8724535315fbe51ebcb19ee7496
MendelDdS <mendelssohn.sa@ccc.ufcg.edu.br>
2015-11-26 00:09:20 -0300

    Us 3 concluída

--- app/models/OrganizaPorMaisRecentes.java
+++ app/models/OrganizaPorMaisRecentes.java
@@ -1,17 +1,15 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class OrganizaPorMaisRecentes implements Organizador {
 
 	@Override
 	public List<Dica> sort(List<Dica> dicas) {
-		List<Dica> ultimasDicas = new ArrayList<>();
-		for (int i = 0; i < dicas.size(); i++) {
-			ultimasDicas.add(dicas.get(dicas.size()-1-i));
-		}
-		dicas = ultimasDicas;
+		Collections.reverse(dicas);
 		return dicas;
 
 	}
